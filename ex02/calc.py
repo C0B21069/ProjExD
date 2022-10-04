77e65ef4b5d64602b1a7a6ec829322da1276e686
Noboru <c0b2106965@edu.teu.ac.jp>
2022-10-04 16:38:21 +0900

    計算機

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,8 +4,7 @@ import tkinter.messagebox as tkm
 
 root = tk.Tk()
 root.title("計算機")
-root.geometry("300x500")
-
+root.geometry("400x600")
 
 def btn_clc(event):
     btn = event.widget
@@ -21,13 +20,19 @@ def btn_kotae(event):
     entry.insert(tk.END,res)
 
 
+def cleare(event):
+    kotae = entry.get()
+    res = eval(kotae)
+    entry.delete(0,tk.END)
+
+
 entry = tk.Entry(root, width=10, font=(", 40"), justify="right") 
-entry.grid(row=0, column=0, columnspan=3)
+entry.grid(row=0, column=0, columnspan=5)
 
 
-r,c = 1,0
+r,c = 2,0
 for i in range(9,-1,-1):
-    btn = tk.Button(root,text=f"{i}",font=(" ",30),width=4,height=2)
+    btn = tk.Button(root,text=f"{i}",font=(" ",30),fg="white",bg="lightgray",width=4,height=2)
     btn .bind("<1>",btn_clc)
     btn.grid(row=r,column=c)
     c += 1
@@ -36,14 +41,25 @@ for i in range(9,-1,-1):
         c = 0
 
 
-btn = tk.Button(root,text=f"+",font=(" ",30),width=4,height=2)
-btn.bind("<1>",btn_clc)
-btn.grid(row=r,column=c)
-
-
-btn = tk.Button(root,text=f"=",font=(" ",30),width=4,height=2)
+btn = tk.Button(root,text=f"=",font=(" ",30),bg="LightSteelBlue1",width=8,height=2)
 btn.bind("<1>",btn_kotae)
-btn.grid(row=r,column=c+1)
+btn.grid(row=r,column=c,columnspan=2)
+
+
+btn = tk.Button(root,text=f"AC",font=(" ",30),fg="white",bg="red",width=4,height=2)
+btn.bind("<1>",cleare)
+btn.grid(row=1,column=0)
+
+
+enzansi = ["+","-","*","/"]
+a,b=2,3
+for i in enzansi:
+    btn = tk.Button(root,text=f"{i}",font=(" ",30),fg="White",bg="orange",width=4,height=2)
+    btn.bind("<1>",btn_clc)
+    btn.grid(row=a,column=b)
+    a += 1
+
+
 
 
 root.mainloop()
