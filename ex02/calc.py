1cda78031b7a0126df9aa3f1bce6d594050abb21
Noboru <c0b2106965@edu.teu.ac.jp>
2022-10-04 14:45:05 +0900

    計算機

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,13 +1,22 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+
 root = tk.Tk()
 root.title("計算機")
 root.geometry("300x500")
 
+
+def btn_clc(event):
+    btn = event.widget
+    txt = int(btn["text"])
+    tkm.showinfo(txt,f"{txt}のボタンがクリックされました")
+
+
 r,c = 0,0
 for i in range(9,-1,-1):
     btn = tk.Button(root,text=f"{i}",font=(" ",30),width=4,height=2)
+    btn .bind("<1>",btn_clc)
     btn.grid(row=r,column=c)
     c += 1
     if i%3 == 1:
