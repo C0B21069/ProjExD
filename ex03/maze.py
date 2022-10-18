4a70d3f170cb2d7f620d138f9ac26cedffea61eb
Noboru <c0b2106965@edu.teu.ac.jp>
2022-10-18 15:30:30 +0900

    ３限基本機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -14,20 +14,29 @@ def key_up(event):
     key = " "
 
 
-#練習7
+#練習7,11,12
 def main_proc():
+    global mx, my 
     global cx, cy
     if key == "Up":
-        cy -= 20
+        my -= 1
     if key == "Down":
-        cy += 20
+        my += 1
     if key == "Left":
-        cx -= 20
+        mx -= 1
     if key == "Right":
-        cx += 20
-    # if key == "o": 右上
-    #     cy -= 20
-    #     cx += 20
+        mx += 1
+    if maze_lst[my][mx] == 0:
+        cx, cy = (mx*100)+50 , (my*100)+50
+    else:
+        if key == "Up":
+            my += 1
+        if key == "Down":
+            my -= 1
+        if key == "Left":
+            mx += 1
+        if key == "Right":
+            mx -= 1
     canv.coords("tori", cx, cy)
     root.after(100,main_proc)
 
@@ -41,8 +50,16 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack() 
 
+    #練習9
+    maze_lst = mm.make_maze(15,9)
+    # print(maze_lst) #1:壁/0:床
+
+    #練習10
+    mm.show_maze(canv,maze_lst)
+
     #練習3
     tori = tk.PhotoImage(file="fig/3.png")
+    mx , my = 1, 1
     cx, cy = 300, 400
     canv.create_image(cx, cy, image=tori, tag="tori") 
 
@@ -56,12 +73,5 @@ if __name__ == "__main__":
     #練習7
     main_proc()
 
-    #練習9
-    maze_lst = mm.make_maze(15,9)
-    # print(maze_lst) #1:壁/0:床
-
-    #練習10
-    mm.show_maze(canv,maze_lst)
-    
     root.mainloop()
 
