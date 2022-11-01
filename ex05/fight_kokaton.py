cfd059edd0fd89d07c602eaf89c37781b18e08fe
Noboru <c0b2106965@edu.teu.ac.jp>
2022-11-01 17:21:49 +0900

    完成版

--- ex05/fight_kokaton.py
+++ ex05/fight_kokaton.py
@@ -8,6 +8,8 @@ import tkinter.messagebox as tkm
 
 MAX_SHOTS = 100 #こうかとんが撃てる弾の最大数
 SCORE = 0 #スコアの表示
+
+
 class MySprite(pg.sprite.Sprite): #画像アニメーション
     def __init__(self):
         super(MySprite, self).__init__()
